7d4ce181620309de004c9e3b482250fe76187595
kartoshka <Cmonya.heh@gmail.com>
2026-09-16 13:05:55 +0300

    Добавил фотки кода

--- scr/lab01/03_discount_vat.py
+++ scr/lab01/03_discount_vat.py
@@ -1,6 +1,6 @@
-price = float(input())
-discount = float(input())
-vat = float(input())
+price = float(input('Price: '))
+discount = float(input('Discount: '))
+vat = float(input('Vat: '))
 base = price * (1 - discount/100)
 vat_amount = base * (vat/100)
 total = base + vat_amount
--- scr/lab01/04_minutes_to_hhmm.py
+++ scr/lab01/04_minutes_to_hhmm.py
@@ -1,4 +1,6 @@
 minutes = int(input('Минуты: '))
+if minutes > 1440:
+    raise ValueError('Слишком много минут')
 hours = minutes // 60
 minutes = minutes - hours * 60
 print(f'{hours}:{minutes}')
