6c8acbd584467834ca2d365603ad0e2fb4ea46f6
kartoshka <Cmonya.heh@gmail.com>
2026-09-16 13:45:52 +0300

    Исправление 5 задания

--- scr/lab01/05_initials_and_len.py
+++ scr/lab01/05_initials_and_len.py
@@ -1,4 +1,4 @@
-full_name = input().split()
-lenght = len(''.join(full_name))
-name = ''.join([i[0].upper() for i in full_name])
+full_name = input('ФИО: ').split()
+lenght = len(''.join(full_name))+2
+name = ''.join([i[0].upper() for i in full_name]) + '.'
 print(f'Инициалы: {name}\nДлина: {lenght}')
